908f30c83d9fef6e37056fca29df2346ee05ac63
Diego <dasalazar90@misena.edu.co>
2020-09-08 21:30:51 -0500

    Correcion de un merge
    Me Equivoque a la hora del conflicto, porque fusione el archivo actual de mi rama con los cambios que traía el merge

--- SAEL/src/main/java/co/edu/sena/sael/vista/ErrorVista.java
+++ SAEL/src/main/java/co/edu/sena/sael/vista/ErrorVista.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -36,42 +35,4 @@ public class ErrorVista implements Serializable {
     
 }
 
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package co.edu.sena.sael.vista;
 
-import java.io.PrintWriter;
-import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
-import java.io.Serializable;
-import java.io.StringWriter;
-
-/**
- * ErrorVista
- * @version 1
- * Fecha de última modificación: 
- * @author grupo 6
- */
-@Named(value = "errorVista")
-@SessionScoped
-public class ErrorVista implements Serializable {
-
-    /**
-     * Creates a new instance of ErrorVista
-     */
-    public ErrorVista() {
-    }
-    
-    public String printStackTrace(Throwable exception){
-        StringWriter stringWriter = new StringWriter();
-        exception.printStackTrace(new PrintWriter(stringWriter, true));
-        return stringWriter.toString();
-    }
-    
-}
-
->>>>>>> v1
