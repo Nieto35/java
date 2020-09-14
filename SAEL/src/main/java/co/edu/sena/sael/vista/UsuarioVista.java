8e58b980ac23476343f8a7d686d66a99175c3f3d
Diego <dasalazar90@misena.edu.co>
2020-09-13 21:02:49 -0500

    Validar Sesión
    resolvimos unos errores del grupo g5, y Se solucionaron los errores con el validar sesión, ya que estaba incompleto.

--- SAEL/src/main/java/co/edu/sena/sael/vista/UsuarioVista.java
+++ SAEL/src/main/java/co/edu/sena/sael/vista/UsuarioVista.java
@@ -15,9 +15,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import org.primefaces.component.inputtext.InputText;
 import org.primefaces.component.password.Password;
 import org.primefaces.component.selectonemenu.SelectOneMenu;
@@ -26,12 +29,16 @@ import org.primefaces.component.selectonemenu.SelectOneMenu;
  *
  * @author Felipe
  */
-
+@SessionScoped
 public class UsuarioVista implements Serializable{
     @EJB
     private UsuarioLogicaLocal usuariosLogica;
     @EJB
     private PersonalLogicaLocal personalLogica;
+    @EJB
+    private UsuarioLogicaLocal usuarioLogica;
+    @Inject
+    private HttpServletRequest request;
     
     private InputText txtUsuario;
     private Password passUsuario;
@@ -164,22 +171,34 @@ public class UsuarioVista implements Serializable{
     }
     
     public void validarSesion()
-    {
-        try {
-            ExternalContext context=FacesContext.getCurrentInstance().getExternalContext();
-            usuarioLogeado = (Personal) context.getSessionMap().get("usuarioLogeado");
+{
+try {
+	String url = request.getRequestURL().toString();
+	System.out.println("url: "+url);
+	ExternalContext context=FacesContext.getCurrentInstance().getExternalContext();
+	usuarioLogeado = (Personal) context.getSessionMap().get("usuarioLogeado"); 
+	String ctxPath = ((ServletContext) context.getContext()).getContextPath();
 
-            if(usuarioLogeado==null)
-            {                
-                String ctxPath = ((ServletContext) context.getContext()).getContextPath();
-                context.redirect(ctxPath+ "/faces/SesionInvalida.xhtml");
-                
-            }
-        } catch (IOException e) {
-            Logger.getLogger(UsuarioVista.class.getName()).log(Level.SEVERE, null, e);
-        }
-        
-    }
+
+	//si la pagina actual es el login o la configurada como inicial
+	if(url.endsWith ("/SAEL/") || url.endsWith("/index.xhtml"))
+	{
+		//si hay una sesión iniciada redirije al index siempre y  no permite ir al login
+		if(usuarioLogeado != null)
+		{
+			context.redirect(ctxPath+ "/indexInstructor.xhtml");
+		}
+	}
+	else if(usuarioLogeado==null)//si es otra pagina y  no hay sesion ·niciada abre sesion invalida
+	{
+
+		context.redirect(ctxPath+ "/SesionInvalida.xhtml");
+	}
+      }catch (IOException e) {
+	Logger.getLogger(UsuarioVista.class.getName()).log(Level.SEVERE, null, e);
+	}
+}
+
     
     public void cambiarPassAction()
     {
